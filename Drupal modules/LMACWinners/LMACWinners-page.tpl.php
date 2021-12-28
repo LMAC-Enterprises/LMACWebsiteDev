bb6673125889a8984d47800d0a3d2d827ad4dffb
quantumg <coding4food@mail.de>
2021-12-28 19:10:54 +0100

    Added grouping by Top categories
    Added grouping of winner collages by top categories.

--- Drupal modules/LMACWinners/LMACWinners-page.tpl.php
+++ Drupal modules/LMACWinners/LMACWinners-page.tpl.php
@@ -45,20 +45,29 @@
 			</div>
 		<?php } ?>
 		<h3><?php print t('Winners'); ?></h3>
-		<?php foreach($contest->winners as $winner) { ?>
-		<div class="lmac-contest-winner-thumbnail-box">
-			<div class="lmac-contest-winner-thumbnail-footer" class="lmac-winner-place-<?php print $winner['winningPlace']; ?>"><span class="lmac-winner-place-label-<?php print $winner['winningPlace']; ?>"><?php print $winner['placeText']; ?> - By </span><a class="lmac-winner-artist-label" href="https://peakd.com/@<?php print $winner['artist']; ?>" title="<?php print t('Click to open the Hive profile of the artist'); ?>">@<?php print $winner['artist']; ?></a>
-			</div>
-         <div class="lmac-contest-winner-thumbnail-box-image-container">
-			   <img src="<?php print $winner['imageThumbnailUrl']; ?>" alt="Image" data-full="<?php print $winner['imageUrl']; ?>">
-         </div>
-			<div class="lmac-contest-winner-dapps-box">
-				<?php foreach ($winner['dappyfiedPostUrls'] as $dappKey => $dappyfiedPostUrl) { ?> 
-				<a href="<?php print $dappyfiedPostUrl; ?>" title="<?php print t('Open entry in this specific dapp.'); ?>" target="_blank" class="lmac-winner-dapp-link <?php print $dappKey; ?>-dapp-icon"></a>
-				<?php } ?>
-			</div>
-		</div>
-		<?php } ?>
+      <p><?php print t('Here you can find the winners of this round. Click on an image to see it in a bigger size. You can visit the original Hive post of an image by clicking on the Dapp icons.'); ?></p>
+      <?php foreach($contest->groupedWinners as $winnerGroup=>$winners) { ?>
+         <br />
+         <h4><?php print t('Top !group', array('!group'=>$winnerGroup)); ?></h4>
+         <?php foreach($winners as $winner) { ?>
+		   <div class="lmac-contest-winner-thumbnail-box">
+			   <div class="lmac-contest-winner-thumbnail-footer" class="lmac-winner-place-<?php print $winner['winningPlace']; ?>"><span class="lmac-winner-place-label-<?php print $winner['winningPlace']; ?>"><?php print $winner['placeText']; ?> - By </span><a class="lmac-winner-artist-label" href="https://peakd.com/@<?php print $winner['artist']; ?>" title="<?php print t('Click to open the Hive profile of the artist'); ?>">@<?php print $winner['artist']; ?></a>
+			   </div>
+            <div class="lmac-contest-winner-thumbnail-box-image-container">
+			      <img src="<?php print $winner['imageThumbnailUrl']; ?>" alt="Image" data-full="<?php print $winner['imageUrl']; ?>">
+            </div>
+			   <div class="lmac-contest-winner-dapps-box">
+				   <?php foreach ($winner['dappyfiedPostUrls'] as $dappKey => $dappyfiedPostUrl) { ?> 
+				   <a href="<?php print $dappyfiedPostUrl; ?>" title="<?php print t('Open entry in this specific dapp.'); ?>" target="_blank" class="lmac-winner-dapp-link <?php print $dappKey; ?>-dapp-icon"></a>
+				   <?php } ?>
+			   </div>
+		   </div>
+      <?php } 
+            if (empty($winners)) {
+      ?>
+      <p><?php print t('No winners in this group.'); ?></p>
+          <?php } ?>  
+      <?php } ?>
 	</div>
 	<div id="lmac-contest-navigation" class="footer-nav">		
 		<?php if (! empty($navigation['previousRound'])) { ?><a href="<?php print $navigation['previousRound']; ?>" title="<?php print t('Previous contest round'); ?>"><?php print t('Previous'); ?></a><?php }else{ ?><span><?php print t('Previous'); ?></span><?php } ?>&nbsp;&bull;&nbsp;
